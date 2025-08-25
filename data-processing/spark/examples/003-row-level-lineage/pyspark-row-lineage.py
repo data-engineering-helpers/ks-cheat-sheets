2b50a252df880990cf53c99e0493c20ec3a2f9b5
Ronan Fruit <ronan.fruit@decathlon.com>
2025-08-25 23:26:28 +0200

    feat: finalized implementation of row_lineage_with_column and started to fix row_lineage_select

--- data-processing/spark/examples/003-row-level-lineage/pyspark-row-lineage.py
+++ data-processing/spark/examples/003-row-level-lineage/pyspark-row-lineage.py
@@ -1,5 +1,6 @@
 import re
 import typing as T
+import functools
 
 import pyspark.sql.functions as F
 from pyspark.sql import (
@@ -17,14 +18,38 @@ from functools import reduce
 ColumnOrName = T.Union[Column, str]
 
 
-def column_expr_validator(col_expr: ColumnOrName) -> str:
-    match col_expr:
+def column_expr_validator(col: ColumnOrName) -> str:
+    match col:
         case str():
-            return col_expr
+            return col
         case Column():
-            return col_expr._jc.toString()
+            return str(col._jc)
         case _:
-            raise ValueError(f"Invalid column expression: {col_expr}")
+            raise ValueError(f"Invalid column expression: {col}")
+
+
+def find_closing_parenthesis_idx(s: str) -> int:
+    if s[0] != '(':
+        return -1
+    stack = 0
+    for i, c in enumerate(s):
+        if c == '(':
+            stack += 1
+        elif c == ')':
+            stack -= 1
+            if stack == 0:
+                return i
+    return -1
+
+
+def find_partitions_expr(col: ColumnOrName) -> T.List[str]:
+    col = column_expr_validator(col)
+    over_positions = list(re.finditer(r' OVER ', col))
+    l = []
+    for i, _ in enumerate(over_positions):
+        j = find_closing_parenthesis_idx(col[over_positions[i].end():over_positions[i+1].start() if over_positions[i+1:] else None])
+        l.append(col[over_positions[i].start():over_positions[i].end() + j + 1])
+    return l
 
 
 def row_ids_column(df: DataFrame) -> str:
@@ -50,10 +75,22 @@ def pyspark_monkey_patch(*row_ids_columns: str):
     #################################
     # Aggregation column
     # -------------------------------
-    def get_aggregation_column(row_ids_col: str) -> Column:
+    def get_aggregation_column(row_ids_col: ColumnOrName) -> Column:
         return F.array_distinct(
             F.flatten(
                 F.collect_list(row_ids_col)
+            )
+        )
+    
+    def get_aggregation_expr(row_ids_col: ColumnOrName) -> str:
+        return str(get_aggregation_column(row_ids_col)._jc)
+    
+    def aggregate_row_ids(col: ColumnOrName, row_ids_col: str) -> Column:
+        return F.expr(
+            functools.reduce(
+                lambda c, partition: f"({get_aggregation_expr(c)}){partition}",
+                find_partitions_expr(col),
+                row_ids_col
             )
         ).alias(row_ids_col)
 
@@ -63,7 +100,7 @@ def pyspark_monkey_patch(*row_ids_columns: str):
         return self.agg(
             *exprs,  # TODO: expressions can contain nested window functions
             *[
-                get_aggregation_column(row_ids_col) 
+                get_aggregation_column(row_ids_col).alias(row_ids_col)
                 for row_ids_col in set(self._df.columns) & set(row_ids_columns)
             ]
         )
@@ -75,43 +112,36 @@ def pyspark_monkey_patch(*row_ids_columns: str):
     # Select
     # -------------------------------
     def row_lineage_select(self: DataFrame, *cols: ColumnOrName) -> DataFrame:
-        return self.select(
+        partitioned_columns = [col for col in cols if "OVER" in column_expr_validator(col)]
+        output_df = self.select(
             *cols,
-            *[row_ids_col for row_ids_col in set(self.columns) & set(row_ids_columns)]
+            *[row_ids_col for row_ids_col in set(self.columns) & set(row_ids_columns)],
         )
+        if partitioned_columns:
+            return functools.reduce(
+                lambda df, partitioned_col: ...,  # TODO: implement
+                partitioned_columns,
+                output_df
+            )
+        return output_df
     DataFrame.select = row_lineage_select
-    # TODO: window function can be used within select statement
+    # TODO: selectExpr
     # TODO: aggregation functions can be used without groupBy or window functions e.g., df.select(F.sum("col1"))
 
     # withColumn
     # -------------------------------
-    def replace_window_agg_expr(window_expr: ColumnOrName, new_agg_expr: ColumnOrName):
-        # The regex pattern:
-        # \b(\w+)\b : Matches and captures a word (group 1)
-        # (?=\sOVER\s\(.+\)) : Positive lookahead to ensure it's followed by " OVER (some_text)"
-        pattern = r"\b(\w+)\b(?=\sOVER\s\(.+\))"
-        # Use re.sub to replace the captured word
-        return re.sub(
-            pattern,
-            column_expr_validator(new_agg_expr), 
-            column_expr_validator(window_expr)
-        )
-    
     def row_lineage_with_column(self: DataFrame, colName: str, col: Column) -> DataFrame:
-        col_sql_expr = str(col._jc.toString())
-        if "PARTITION BY" in col_sql_expr:
-            return reduce(
-                lambda df, row_ids_col: df.withColumn(
-                    row_ids_col, 
-                    F.expr(replace_window_agg_expr(col_sql_expr, get_aggregation_column(row_ids_col))).alias(row_ids_col)
-                ),
-                set(self.columns) & set(row_ids_columns), 
+        if "OVER" in str(col._jc):
+            return functools.reduce(
+                lambda df, row_id_col: df.withColumn(row_id_col, aggregate_row_ids(colName, row_id_col)),
+                row_ids_columns,
                 self.withColumn(colName, col)
             )
         return self.withColumn(colName, col)
     DataFrame.withColumn = row_lineage_with_column
 
-
-    # TODO: leftanti / leftsemi could be problematic...
-
-    
+    #################################
+    # Joins
+    #################################
+    # TODO: when joining 2 dataframes, the same row_ids_columns could be present in both => we should take the union!
+    # TODO: leftanti / leftsemi could be problematic...    
