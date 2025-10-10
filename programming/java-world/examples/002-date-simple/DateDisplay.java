341b384f5813d310737e9dbfa3a1c1baa2b63f51
Denis Arnaud <denis.arnaud@decathlon.com>
2025-10-10 19:18:30 +0200

    [Java] Added a simple example with dates

---
+++ programming/java-world/examples/002-date-simple/DateDisplay.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class DateDisplay {
+    
+    // Your program begins with a call to main()
+    public static void main(String[] args)
+    {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println(new Date(9103000000L));
+    }
+}
+
